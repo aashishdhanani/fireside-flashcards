4c2ace5ff572de7c531f18b7b73cde3f14cd641b
Waleedprw22 <wk2383@columbia.edu>
2024-08-19 21:36:22 -0400

    merge issue

--- app/generate/page.js
+++ app/generate/page.js
@@ -18,12 +18,55 @@ export default function Generate() {
     const router = useRouter()
 
     useEffect(() => {
+<<<<<<< Updated upstream
         if (isLoaded && !isSignedIn) {
             router.push('/sign-in') // Redirect to sign-in page if not signed in
+=======
+        if (isLoaded) {
+            if (!isSignedIn) {
+                // Redirect immediately if not signed in
+                router.push('/sign-in')
+                return
+            }
+
+            const checkPaidStatus = async () => {
+                try {
+                    const userDocRef = doc(db, 'users', user.id);
+                    const docSnap = await getDoc(userDocRef);
+
+                    if (docSnap.exists()) {
+                        const userData = docSnap.data();
+                        if (userData.paidUser) {
+                            setIsAuthorized(true); // Allow page to render
+                        } else {
+                            setIsAuthorized(false);
+                        }
+                    } else {
+                        setIsAuthorized(false);
+                    }
+                } catch (error) {
+                    console.error("Error checking user status:", error);
+                    setIsAuthorized(false);
+                }
+            };
+
+            checkPaidStatus();
+>>>>>>> Stashed changes
         }
     }, [isLoaded, isSignedIn, router])
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
     if (!isLoaded || !isSignedIn) {
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+    `
+    if (!isAuthorized) {
+>>>>>>> Stashed changes
         return (
             <Box
                 sx={{
@@ -44,6 +87,22 @@ export default function Generate() {
             </Box>
         )
     }
+        `
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> Stashed changes
+
+    // const handleSubmit = async () => {
+    //     fetch('api/generate', {
+    //         method: 'POST',
+    //         body: JSON.stringify({ userId: user.id }),
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => setFlashcards(data))
+    // }
+>>>>>>> Stashed changes
 
     const handleSubmit = async () => {
         fetch('api/generate', {
@@ -105,26 +164,26 @@ export default function Generate() {
 
     return (
         <Container maxWidth="100vw" sx={{ backgroundColor: '#2E2E2E', minHeight: '100vh', p: 0 }}>
-            <AppBar position="static" sx={{ backgroundColor: '#2E2E2E'}}>
+            <AppBar position="static" sx={{ backgroundColor: '#2E2E2E' }}>
                 <Toolbar>
-                <Link href="/" passHref style={{ flexGrow: 1, textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}> 
-                    <Typography 
-                    variant="h6"  
-                    style={{ flexGrow: 1, textDecoration: 'none', color: '#FCD19C', cursor: 'pointer' }}
-                    >
-                    Fireside Flashcards
-                    </Typography>
-                </Link>
-                <Button color="inherit" href="/profile">Profile</Button>
-                <Button color="inherit" href="/generate">Generate</Button>
-                <Button color="inherit" href="/flashcards">Flashcards</Button>
-                <SignedOut>
-                    <Button color="inherit" href="/sign-in">Login</Button>
-                    <Button color="inherit" href="/sign-up">Sign Up</Button>
-                </SignedOut>
-                <SignedIn>
-                    <UserButton />
-                </SignedIn>
+                    <Link href="/" passHref style={{ flexGrow: 1, textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}> 
+                        <Typography 
+                            variant="h6"  
+                            style={{ flexGrow: 1, textDecoration: 'none', color: '#FCD19C', cursor: 'pointer' }}
+                        >
+                            Fireside Flashcards
+                        </Typography>
+                    </Link>
+                    <Button color="inherit" href="/profile">Profile</Button>
+                    <Button color="inherit" href="/generate">Generate</Button>
+                    <Button color="inherit" href="/flashcards">Flashcards</Button>
+                    <SignedOut>
+                        <Button color="inherit" href="/sign-in">Login</Button>
+                        <Button color="inherit" href="/sign-up">Sign Up</Button>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
                 </Toolbar>
             </AppBar>
             <Box
@@ -139,6 +198,9 @@ export default function Generate() {
                 <Typography variant="h4" sx={{ mb: 4, color: 'white' }}>
                     Generate Flashcards
                 </Typography>
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
                 <Paper sx={{ p: 4, width: '100%' }}>
                     <TextField
                         value={text}
@@ -231,32 +293,162 @@ export default function Generate() {
                     <Box sx={{ mt: 4, display: 'flex', justifyContent: "center" }}>
                         <Button variant='contained' color='secondary' onClick={handleOpen}>
                             Save
+=======
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+    
+                {!isAuthorized && (
+                    <>
+                        <Typography variant="h6" sx={{ color: 'red', mt: 2 }}>
+                            This may only be accessed by Pro Plan Members.
+                        </Typography>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            onClick={() => router.push('/payment')}
+                            sx={{ backgroundColor: '#FCD19C', color: '#000', '&:hover': { backgroundColor: '#e0a44d' }, mt: 2 }}
+                        >
+                            Upgrade to Pro Plan
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
                         </Button>
-                    </Box>
-                </Box>
-            )}
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>Save Flashcards</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Please enter a name for your flashcards collection
-                    </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin='dense'
-                        label="Collection Name"
-                        type="text"
-                        fullWidth
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        variant="outlined"
-                    />
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={saveFlashcards}>Save</Button>
-                </DialogActions>
-            </Dialog>
+                    </>
+                )}
+    
+                {isAuthorized && (
+                    <>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={handleSubmit}
+                            sx={{ backgroundColor: '#FCD19C', color: '#000', '&:hover': { backgroundColor: '#e0a44d' }, mt: 2 }}
+                        >
+                            Generate
+                        </Button>
+    
+                        {generating && (  // Show loading spinner while generating flashcards
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    minHeight: '100px',
+                                    backgroundColor: '#2E2E2E',
+                                    color: '#FCD19C',
+                                    textAlign: 'center'
+                                }}
+                            >
+                                <CircularProgress sx={{ color: '#FCD19C', mb: 2 }} />
+                                <Typography variant="h6" sx={{ ml: 2 }}>
+                                    Generating flashcards...
+                                </Typography>
+                            </Box>
+                        )}
+    
+                        {flashcards.length > 0 && !generating && (
+                            <Box sx={{ mt: 4 }}>
+                                <Typography variant="h5" sx={{ color: "white", my: 4, textAlign: "center" }}>
+                                    Flashcards Preview
+                                </Typography>
+                                <Grid container spacing={3}>
+                                    {flashcards.map((flashcard, index) => (
+                                        <Grid item xs={12} sm={6} md={4} key={index}>
+                                            <Card>
+                                                <CardActionArea
+                                                    onClick={() => {
+                                                        handleCardClick(index)
+                                                    }}
+                                                >
+                                                    <CardContent>
+                                                        <Box
+                                                            sx={{
+                                                                perspective: '1000px',
+                                                                '& > div': {
+                                                                    transition: 'transform 0.6s',
+                                                                    transformStyle: 'preserve-3d',
+                                                                    position: 'relative',
+                                                                    width: '100%',
+                                                                    height: '300px', // Increased height
+                                                                    maxWidth: '800px', // Increased max width
+                                                                    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
+                                                                    transform: flipped[index] ? 'rotateY(180deg)' : 'rotateY(0deg)',
+                                                                    margin: 'auto', // Center the card horizontally
+                                                                },
+                                                                '& > div > div': {
+                                                                    position: 'absolute',
+                                                                    width: '100%',
+                                                                    height: '100%',
+                                                                    backfaceVisibility: 'hidden',
+                                                                    display: 'flex',
+                                                                    justifyContent: 'center',
+                                                                    alignItems: 'center',
+                                                                    padding: 2,
+                                                                    boxSizing: 'border-box',
+                                                                },
+                                                                '& > div > div:nth-of-type(2)': {
+                                                                    transform: 'rotateY(180deg)',
+                                                                },
+                                                            }}
+                                                        >
+                                                            <div>
+                                                                <div>
+                                                                    <Typography variant="h5" component="div" sx={{ textAlign: 'center' }}>
+                                                                        {flashcard.front}
+                                                                    </Typography>
+                                                                </div>
+                                                                <div>
+                                                                    <Typography variant="h8" component="div" sx={{ textAlign: 'center' }}>
+                                                                        {flashcard.back}
+                                                                    </Typography>
+                                                                </div>
+                                                            </div>
+                                                        </Box>
+                                                    </CardContent>
+                                                </CardActionArea>
+                                            </Card>
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                                <Box sx={{ mt: 4, display: 'flex', justifyContent: "center" }}>
+                                    <Button variant='contained' color='secondary' onClick={handleOpen}>
+                                        Save
+                                    </Button>
+                                </Box>
+                            </Box>
+                        )}
+                    </>
+                )}
+    
+                <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Save Flashcards</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please enter a name for your flashcards collection
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin='dense'
+                            label="Collection Name"
+                            type="text"
+                            fullWidth
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                            variant="outlined"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={saveFlashcards}>Save</Button>
+                    </DialogActions>
+                </Dialog>
+            </Box>
         </Container>
     )
-}
+}    
